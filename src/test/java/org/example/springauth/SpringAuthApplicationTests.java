e940f2fd5c822f08e0fd4348fe32955da25c409e
tigerpoint123 <tigerrla@naver.com>
2024-12-23 09:28:32 +0900

    crete project

---
+++ src/test/java/org/example/springauth/SpringAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
